765ca04a0a9f7e190f66aa625a9619dea0ef6857
GnzlzP <gnzlzp.dev@gmail.com>
2025-04-18 10:38:48 -0500

    quite comillas a PUBLIC_CLOUDINARY_CLOUD_NAME

--- .netlify/build/entry.mjs
+++ .netlify/build/entry.mjs
@@ -1,21 +1,23 @@
 import { renderers } from './renderers.mjs';
 import { s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CvSoi7hX.mjs';
-import { manifest } from './manifest_CeGJ-nR4.mjs';
+import { manifest } from './manifest_C7zu8Ui9.mjs';
 import { createExports } from '@astrojs/netlify/ssr-function.js';
 
 const serverIslandMap = new Map();;
 
 const _page0 = () => import('./pages/404.astro.mjs');
 const _page1 = () => import('./pages/contacto.astro.mjs');
-const _page2 = () => import('./pages/nosotros.astro.mjs');
-const _page3 = () => import('./pages/servicios.astro.mjs');
-const _page4 = () => import('./pages/index.astro.mjs');
+const _page2 = () => import('./pages/gracias.astro.mjs');
+const _page3 = () => import('./pages/nosotros.astro.mjs');
+const _page4 = () => import('./pages/servicios.astro.mjs');
+const _page5 = () => import('./pages/index.astro.mjs');
 const pageMap = new Map([
     ["src/pages/404.astro", _page0],
     ["src/pages/contacto.astro", _page1],
-    ["src/pages/nosotros.astro", _page2],
-    ["src/pages/servicios.astro", _page3],
-    ["src/pages/index.astro", _page4]
+    ["src/pages/gracias.astro", _page2],
+    ["src/pages/nosotros.astro", _page3],
+    ["src/pages/servicios.astro", _page4],
+    ["src/pages/index.astro", _page5]
 ]);
 
 const _manifest = Object.assign(manifest, {
@@ -26,7 +28,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "60b3f6d5-73ef-4303-b941-9916de122b18"
+    "middlewareSecret": "f302ebac-9632-4d3f-84e0-e6064a10b013"
 };
 const _exports = createExports(_manifest, _args);
 const __astrojsSsrVirtualEntry = _exports.default;
